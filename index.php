a5323efee5c53e4d609ba62af701cc21ffdd6b80
Ilze Rigerte <irigerte@gmail.com>
2022-08-17 19:42:33 +0300

    very start

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>To Do</title>
+</head>
+
+<body>
+<form action="">
+  <div class="container">
+    <div class="wrapper">
+      <h1>To Do</h1>
+      <div>
+      <input type="text" name="" class="main_input">
+      </div>
+      <div>
+      <button>ADD</button>
+      </div>
+    </div>
+  </div>
+  </form>
+</body>
+
+</html>
